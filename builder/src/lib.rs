2db75a9192b8eec5a5ccb556074144dfdbf259dc
Joseph Micheli <josephwmicheli@gmail.com>
2023-12-27 00:13:50 -0600

    Builder - Pass test 07

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -2,7 +2,7 @@ use proc_macro2::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput, Field, Ident};
 
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
   let input = parse_macro_input!(input as DeriveInput);
 
@@ -46,8 +46,8 @@ fn gen_input_struct_impl(input_struct: &DeriveInput) -> TokenStream {
 }
 
 fn gen_field_initializer(field: &Field) -> TokenStream {
-  let field_identity = field.ident.clone().unwrap();
-  quote! { #field_identity: None }
+  let field_name = field.ident.clone().unwrap();
+  quote! { #field_name: None }
 }
 
 fn gen_builder_struct(input_struct: &DeriveInput) -> TokenStream {
@@ -101,11 +101,14 @@ fn gen_builder_struct_impl(input_struct: &DeriveInput) -> TokenStream {
 
 fn gen_field_assignor(field: &Field) -> TokenStream {
   let field_name = field.ident.clone().unwrap();
+  let field_name_str = field_name.to_string();
   let field_type = field.ty.clone();
   if extract_inner_type(&field_type, "Option").is_some() {
     quote!(#field_name: self.#field_name.clone())
+  } else if extract_inner_type(&field_type, "Vec").is_some() {
+    quote!(#field_name: self.#field_name.clone().unwrap_or(vec![]))
   } else {
-    quote!(#field_name: self.#field_name.clone().ok_or("field was not set")?)
+    quote!(#field_name: self.#field_name.clone().ok_or(format!("Field {} was not set", #field_name_str))?)
   }
 }
 
@@ -118,9 +121,52 @@ fn gen_field_setter_fn(field: &Field) -> TokenStream {
     field_type = inner_ty;
   };
 
+  let normal_setter = gen_normal_setter_fn(&field_name, &field_type);
+
+  // Return immediately if there's no attribute1
+  if field.attrs.is_empty() {
+    normal_setter
+  } else {
+    match extract_each_attr(&field.attrs) {
+      Ok(name) => {
+        let each_setter = gen_each_setter_fn(&field_name, &field_type, &name);
+
+        // Check if we have overlapping each and normal names, if so, only return each, otherwise return both
+        if name == field_name.to_string() {
+          return each_setter;
+        } else {
+          return quote! {
+            #normal_setter
+            #each_setter
+          };
+        }
+      }
+      Err(e) => e,
+    }
+  }
+}
+
+fn gen_normal_setter_fn(field_name: &Ident, field_type: &syn::Type) -> TokenStream {
   quote! {
     pub fn #field_name(&mut self, value: #field_type) -> &mut Self {
       self.#field_name = Some(value);
+      self
+    }
+  }
+}
+
+fn gen_each_setter_fn(field_name: &Ident, field_type: &syn::Type, name: &String) -> TokenStream {
+  let method_name = quote::format_ident!("{}", name);
+  let inner_type = extract_inner_type(field_type, "Vec").expect("Expected Vec container");
+
+  quote! {
+    pub fn #method_name(&mut self, value: #inner_type) -> &mut Self {
+      if let std::option::Option::Some(inner_vec) = &mut self.#field_name {
+        inner_vec.push(value);
+      } else {
+        self.#field_name = std::option::Option::Some(vec![value]);
+      }
+
       self
     }
   }
@@ -160,4 +206,33 @@ fn extract_inner_type(ty: &syn::Type, expected_ident: &str) -> Option<syn::Type>
     }
   }
   None
+}
+
+fn extract_each_attr(attrs: &Vec<syn::Attribute>) -> Result<String, TokenStream> {
+  let mut fn_name = String::new();
+  if let Some(attr) = attrs.last() {
+    match &attr.meta {
+      syn::Meta::List(list) => {
+        list
+          .parse_nested_meta(|meta| {
+            if meta.path.is_ident("each") {
+              let value = meta.value().unwrap();
+              let lit_str: syn::LitStr = value.parse()?; // this parses `"EarlGrey"`
+              fn_name = lit_str.value();
+              Ok(())
+            } else {
+              panic!("You gotta have an each!");
+            }
+          })
+          .unwrap();
+
+        return Ok(fn_name);
+      }
+      _ => {
+        panic!("Only lists can be used as attribute inputs (e.g. #[builder(each = \"fn_name\")!")
+      }
+    }
+  }
+
+  unreachable!("Should never extract from empty attr list.")
 }
